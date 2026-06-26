b549b7808d64e269e0500a68717361a5cd6f925c
Muhammad Faizan Anwar <faizanarian77@gmail.com>
2026-06-26 12:35:58 +0500

    Add files via upload

---
+++ src/main/java/com/vitanet/enums/NotificationType.java
@@ -0,0 +1,10 @@
+package com.vitanet.enums;
+
+/**
+ * Types of in-app notifications sent within VitaNet.
+ */
+public enum NotificationType {
+    NEW_DONATION,
+    NEW_REQUEST,
+    DONATION_FULFILLED
+}
